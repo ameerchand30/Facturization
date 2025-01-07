82b33a92c2950fa87b93c1bc6920a21e067f6dbb
ameerchand30 <ameerchand30@gmail.com>
2025-01-07 19:10:00 +0100

    before trying to get plusier value a server side

--- facturaization/api/models/invoice.py
+++ facturaization/api/models/invoice.py
@@ -17,7 +17,7 @@ class Invoice(Base):
     __tablename__ = 'invoices'
     
     id = Column(Integer, primary_key=True, index=True)
-    client_id = Column(Integer, ForeignKey('clients.id'))
+    client_id = Column(Integer, ForeignKey('clients.id', ondelete='CASCADE'))
     enterprise_id = Column(Integer, ForeignKey('enterprises.id'))
     creation_date = Column(DateTime, default=datetime.datetime.utcnow)
     due_date = Column(DateTime, default=None)
@@ -29,13 +29,13 @@ class Invoice(Base):
     payment_method = Column(SQLAEnum(PaymentMethodEnum))
     client = relationship("Clients", back_populates="invoices")
     enterprises = relationship("Enterprise", back_populates="invoices")
-    invoice_items = relationship("InvoiceItem", back_populates="invoice")
+    invoice_items = relationship("InvoiceItem", back_populates="invoice", cascade="all, delete-orphan")
 
 class InvoiceItem(Base):
     __tablename__ = 'invoice_items'
     
     id = Column(Integer, primary_key=True)
-    invoice_id = Column(Integer, ForeignKey('invoices.id'), index=True)
+    invoice_id = Column(Integer, ForeignKey('invoices.id', ondelete='CASCADE'), index=True)
     product_id = Column(Integer, ForeignKey('product.id'))
     quantity = Column(Integer)
     unit_price = Column(Float)
