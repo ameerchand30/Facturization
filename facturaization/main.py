9cc946171be94d9bf908b4199de0375ae13adb9c
ameerchand30 <ameerchand30@gmail.com>
2025-05-01 00:03:49 +0200

    main update

--- facturaization/main.py
+++ facturaization/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI, Request,APIRouter
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.sessions import SessionMiddleware
 
 # from routers.invoice_router import invoice_router
+from api.routers.auth_router import auth_router
 from api.routers.client_router import client_router
 from api.routers.product_router import product_router
 from api.routers.enterprise_router import enterprise_router
@@ -10,7 +13,6 @@ from api.routers.invoice_router import invoice_router
 from api.routers.report_generate import report_router
 from api.routers.dashboard_routes import dashboard_router
 from api.routers.enterprise_profile_router import enterprise_profile_router
-from api.routers.auth_router import auth
 # models
 from api.models.client import Clients
 from api.models.enterprise import Enterprise
@@ -27,6 +29,12 @@ Base.metadata.create_all(bind=engine)
 # Create the FastAPI app
 app = FastAPI()
 
+
+# Set up CORS middleware (if needed)
+
+# Add session middleware
+app.add_middleware(SessionMiddleware, secret_key="your-secret-key")
+
 # Set up Jinja2 templates
 templates = Jinja2Templates(directory="templates")
 
@@ -35,7 +43,8 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 # Include routers
-app.include_router(auth)
+# app.include_router(auth)
+app.include_router(auth_router)
 app.include_router(client_router)
 app.include_router(product_router)
 app.include_router(enterprise_router)
@@ -48,12 +57,7 @@ app.include_router(enterprise_profile_router)
 @app.get("/")
 async def root(request: Request):
     return templates.TemplateResponse("pages/User/LandingPage/landing-page.html", {"request": request})
-@app.get("/login")
-async def login(request: Request):
-    return templates.TemplateResponse("pages/User/login/login.html", {"request": request})
-@app.get("/register")
-async def register(request: Request):
-    return templates.TemplateResponse("pages/User/login/register.html", {"request": request})
+
 @app.get("/dashboard")
 async def dashboard(request: Request):
     return templates.TemplateResponse("pages/dashboard.html", {"request": request,"current_page": "dashboard"})
