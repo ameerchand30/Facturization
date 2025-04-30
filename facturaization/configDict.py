ec30f28cc9c1d122e50c3aff0b624c586aac9e3f
ameerchand30 <ameerchand30@gmail.com>
2025-04-30 22:08:08 +0200

    configDict

--- facturaization/configDict.py
+++ facturaization/configDict.py
@@ -1,5 +1,5 @@
 from pydantic_settings import BaseSettings
-
+from authlib.integrations.starlette_client import OAuth
 class Setting(BaseSettings):
     host_name: str
     host_port: str
@@ -8,8 +8,38 @@ class Setting(BaseSettings):
     database_password: str
     database_name: str
     database_username: str
-    secret_key: str
     algorithm: str
     access_token_expire_minutes: int
+    # security
+    secret_key: str
+    jwt_secret_key: str
+    session_secret: str
+    # OAuth2 settings
+    google_client_id: str
+    google_client_secret: str
+    github_client_id: str
+    github_client_secret: str
+    linkedin_client_id: str
+    linkedin_client_secret: str
+    facebook_client_id: str
+    facebook_client_secret: str
+    OPENAI_API_KEY: str
+    OPENAI_API_MODEL: str
     class Config:
         env_file = "../.env"
+
+  
+settings = Setting()
+oauth = OAuth()
+# Configure OAuth providers
+oauth.register(
+    name='google',
+    client_id=settings.google_client_id,
+    client_secret=settings.google_client_secret,
+    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
+    client_kwargs={
+        'scope': 'openid email profile https://www.googleapis.com/auth/gmail.readonly'
+    }
+)
+
+# Add other providers similarly
